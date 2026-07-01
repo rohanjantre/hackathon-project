4702b6837776f6f2886d84eabafdd1a978369a4b
Rohan Jantre <rohanjantre2004@gmail.com>
2026-07-01 22:03:54 +0530

    show lots of error

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://127.0.0.1:27017/nestAuth',
+    ),
+    AuthModule,
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ backend/src/user/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  name!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @MinLength(6)
+  password!: string;
+}
